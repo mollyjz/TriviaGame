1426be8e79d236860bb75f41d762883529f9a032
Molly Zurheide <you@example.com>
2018-04-16 22:05:50 -0400

    more updates

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,6 @@
 var timer;
-var userIndex = $("user-index");
 
-var questions = [ //array containing objects with questions, answer choices, and correct answer
+var questions = [
     {
         question: "What is the capital of Syria?",
         answerChoices: ["Benghazi", "Beirut", "Cairo", "Damascus"],
@@ -54,22 +53,35 @@ var questions = [ //array containing objects with questions, answer choices, and
     }
 ]
 
-var userChoices = []; //to store user's choices
-//PUSH VALUE TO SELECTE BUTTON TO ARRAY?????
+var userChoices = [];
 
 var numCorrect = 0;
-var numIncorrect = 0; //WHY DISPLAYING AS 10??????????????*******************************************************
+var numIncorrect = 0;
 var numUnanswered = questions.length - userChoices.length;
 
 function startScreen() { //works
     $("#main-area").html("<button id='start'>Start</button>") //load start button
-    $("#start").on("click", loadQuestions) //load questions function (see below)
-    $("#start").on("click", function(){
+    $("#start").on("click", function() {
         $("#start").hide();
     });
+    $("#start").on("click", loadQuestions) //load questions function (see below)
 }
 
-function loadQuestions() { //works
+function loadQuestionHtml() { //loop works but questions not loading
+    for (var i = 0; i < 10; i++) { //for each question...
+        $("#questions-container").append(questions[i].question); //print question text  ********NOT WORKING********
+         //console.log("Hey")
+         //console.log(questions[2].rightAnswer) //object registering but not being printed
+        for (var x = 0; x < 4; x++) { //for each answer choice...
+             $("#answers-container").append(questions[i].answerChoices); //print answer text
+             $("#answers-container").prepend("<input type='radio' id='radio-buttons'></input>"); //add radio button
+             $("#radio-buttons").val(questions[i].answerChoices[x]); //assign value (answer) to radio button
+             //console.log("Hello")
+         }
+        }
+    }
+
+function loadQuestions() {
     var secondsLeft = 60; //works
     $("#main-area").append("<p>Time left: <span id='timer'></span></p><br>"); //load timer
     timer = setInterval(function() { //timer function--every second...
@@ -81,23 +93,12 @@ function loadQuestions() { //works
             }
     }, 1000);
     $("#done-container").append("<button id='done'>Done</button>"); //load "done" button   
-    for (var i = 0; i < questions.length; i++) { //for each question...NOT WORKING!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-       // $("#questions-container").append("<div id='this-question'></div>"); //add div for each question
-        $("#questions-container").append(questions[i].question + "<br>"); //print question text
-        for (var x = 0; x < 4; x++) { //for each answer choice...****************************************************
-            $("#answers-container").append(questions[i].answerPosition); //print answer text
-            $("#answers-container").prepend("<input type='radio'></input>"); //add radio button
-            $("input").attr("user-index", questions[i].rightAnswer); //assign answer to its corresponding button
-            //var userIndex = $("user-index")
-        }
-    }    
+    loadQuestionHtml();
     $("#done-container").on("click", "#done", function() { //if "done" button is clicked...
     clearInterval(timer); //stop timer
     endGame(); //end game (see endGame function)
 });
 }
-    
-//DO AWAY WITH ANSWER INDEX & USER INDEX AND JUST USE ANSWERS!!!!!!!!!!!!1
 
 function endGame() { //works
     calculate(); 
@@ -106,33 +107,26 @@ function endGame() { //works
     $("#game-done").append("<h4>Correct: " + numCorrect + "</h4>"); //number of correct answers //works
     $("#game-done").append("<h4>Incorrect: " + numIncorrect + "</h4>"); //number of wrong answers
     $("#game-done").append("<h4>Unanswered: " + numUnanswered + "</h4>"); //# of unanswered questions //works
-    $("#game-done").append("<button id='play-again'>Play Again!</button>"); //works
-    resetGame();
+    //$("#game-done").append("<button id='play-again'>Play Again!</button>"); //works
+    //resetGame();
 }
 
-function calculate() { //NOT WORKING!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!********************************************
-    userChoices.push(userIndex); //push index of user's choice to user choices array (but can't access user index here d/t scope???)
-    for (var i = 0; i < questions.length; i++) {
-        if (questions[i].rightAnswer == (userChoices[i])) { //if index of correct answer = index of user choice...
-            numCorrect++ //add 1 to count for correct answers
-        } else if (questions[i].rightAnswer !== (userChoices[i])) {
-            numIncorrect++ //add 1 to count for wrong answers
-        }
+function calculate() { //seems to be working?
+    //console.log("Is this working?")
+    for (var i = 0; i < questions.length; i++) { //loop seems to be running
+        if ($("input[type=radio]:checked")) { //if a radio button is selected, push its value(answer) to 
+            userChoices.push($("#radio-buttons").val());
+        } if (questions[i].rightAnswer == (userChoices[i])) { //if correct answer = user choice...
+            numCorrect++; //add 1 to count for correct answers
+        } if (questions[i].rightAnswer !== (userChoices[i])) {
+            numIncorrect++; //add 1 to count for wrong answers
+        } //this is set to 10 because userChoices is not defined and thus not equal to right answer
+        //console.log("Anyone there?")
     }
-}
-    
-function resetGame() { //********************************************************************************************
-    $("body").on("click", "#play-again", startScreen); //FUNCTION TO RESET GAME: load start screen, questions & answers
-    userChoices = []; //to store user's choices
-    numCorrect = 0;
-    numIncorrect = 0; //WHY DISPLAYING AS 10??????????????*******************
-    numUnanswered = questions.length - userChoices.length;
-    //WHAT ELSE TO RESET ON GAME START?????
 }
 
 ////////////////////////////////////
 
 $(document).ready(function(){
     startScreen(); //load start button & timer
-    //ANY OTHER FUNCTIONS NEEDED HERE?????
 });
